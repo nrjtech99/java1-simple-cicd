2d4171b2feeb503aa59f50923d7b2fca86ba796b
Nara <libforme3@gmail.com>
2025-01-25 13:02:26 -0800

    Added log config to jenkns

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!. CI/CD: " + LocalDate.now());
         System.out.println("CI commit");
+        System.out.println("Added logs to jenkins");
     }
 }
