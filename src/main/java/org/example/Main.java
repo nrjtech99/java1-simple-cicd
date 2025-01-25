f82245dc0136953e352f9282d74cc10b851c11e5
Nara <libforme3@gmail.com>
2025-01-25 11:20:07 -0800

    Modified time

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!. CI/CD: " + LocalDateTime.now());
+        System.out.println("Hello world!. CI/CD: " + LocalDate.now());
     }
 }
