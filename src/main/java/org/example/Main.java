87fb3fb8ed3d1dd1261771f262b4805aa5cb1ce3
Nara <libforme3@gmail.com>
2025-01-25 12:51:07 -0800

    Added ci commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!. CI/CD: " + LocalDate.now());
+        System.out.println("CI commit");
     }
 }
